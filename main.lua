2806505935948b3a3d04967922ea31b7ff90ee52
Yarosbug <scourgegames@gmail.com>
2015-04-27 05:26:10 +0300

    Added real board setup (6x6x2) that controled by ENV.dev variable

--- main.lua
+++ main.lua
@@ -1,51 +1,142 @@
-
-
 --------------------------------------------------------------------
 -- Main
 --------------------------------------------------------------------
-
-boardSize = 6
-cellWidth = display.contentWidth/boardSize
-cellHeight = display.contentWidth/boardSize
-
 local GameController = require "GameController"
 local Board = require "Board"
 local BoardView = require "BoardView"
 local Token = require "Token"
 local TokenView = require "TokenView"
 
-local board = Board:new(boardSize)
-
--- Tokens
-local tokensGroup = display.newGroup( )
-local token = Token:new()
-token.isMaster = true
-board:addToken(1, 1, token)
-TokenView:new(token, tokensGroup)
-
-token = Token:new()
-token.isMaster = true
-token.owner = "red"
-board:addToken(2, 1, token)
-TokenView:new(token, tokensGroup)
-
-token = Token:new()
-token.owner = "blue"
-token.isMaster = true
-token.isHorizontal = false
-board:addToken(3, 3, token)
-TokenView:new(token, tokensGroup)
-
-token = Token:new {
-	owner = "red",
-	isMaster = false,
-	isHorizontal = false
+ENV = {
+	dev = true
 }
-board:addToken(6, 1, token)
-TokenView:new(token, tokensGroup)
+
+boardSize = 6
+cellWidth = display.contentWidth/boardSize
+cellHeight = display.contentWidth/boardSize
+
+local function setup6x6x2()
+	local boardSize = 6
+	cellWidth = display.contentWidth/boardSize
+	cellHeight = display.contentWidth/boardSize
+	
+	local board = Board:new(6)
+
+	-- TOP
+	token = Token:new()
+	token.owner = "red"
+	token.isHorizontal = false
+	board:addToken(2, 1, token)
+
+	token = Token:new()
+	token.owner = "blue"
+	token.isHorizontal = false
+	board:addToken(3, 1, token)
+
+	token = Token:new()
+	token.owner = "red"
+	token.isHorizontal = false
+	board:addToken(4, 1, token)
+
+	token = Token:new()
+	token.owner = "blue"
+	token.isHorizontal = false
+	board:addToken(5, 1, token)
+
+	-- RIGHT
+	token = Token:new()
+	token.owner = "blue"
+	board:addToken(6, 2, token)
+
+	token = Token:new()
+	token.owner = "red"
+	board:addToken(6, 3, token)
+
+	token = Token:new()
+	token.owner = "blue"
+	board:addToken(6, 4, token)
+
+	token = Token:new()
+	token.owner = "red"
+	board:addToken(6, 5, token)
+
+	-- BOTTOM
+	token = Token:new()
+	token.owner = "red"
+	token.isHorizontal = false
+	board:addToken(2, 6, token)
+
+	token = Token:new()
+	token.owner = "blue"
+	token.isHorizontal = false
+	board:addToken(3, 6, token)
+
+	token = Token:new()
+	token.owner = "red"
+	token.isHorizontal = false
+	board:addToken(4, 6, token)
+
+	token = Token:new()
+	token.owner = "blue"
+	token.isHorizontal = false
+	board:addToken(5, 6, token)
+
+	-- LEFT
+	token = Token:new()
+	token.owner = "blue"
+	board:addToken(1, 2, token)
+
+	token = Token:new()
+	token.owner = "red"
+	board:addToken(1, 3, token)
+
+	token = Token:new()
+	token.owner = "blue"
+	board:addToken(1, 4, token)
+
+	token = Token:new()
+	token.owner = "red"
+	board:addToken(1, 5, token)
+
+	return board
+end
+
+local board = nil
+
+if ENV.dev then
+	board = Board:new(boardSize)
+	local token = Token:new()
+	token.isMaster = true
+	board:addToken(1, 1, token)
+
+	token = Token:new()
+	token.isMaster = true
+	token.owner = "red"
+	board:addToken(2, 1, token)
+
+	token = Token:new()
+	token.owner = "blue"
+	token.isMaster = true
+	token.isHorizontal = false
+	board:addToken(3, 3, token)
+
+	token = Token:new {
+		owner = "red",
+		isMaster = false,
+		isHorizontal = false
+	}
+	board:addToken(6, 1, token)
+else
+	board = setup6x6x2()
+end
 
 local boardView = BoardView:new(board)
 local boardGroup = boardView.group
+
+local tokensGroup = display.newGroup( )
+for _, token in ipairs(board:getAllTokens()) do
+	TokenView:new(token, tokensGroup)
+end
 
 GameController:new(boardView)
 
